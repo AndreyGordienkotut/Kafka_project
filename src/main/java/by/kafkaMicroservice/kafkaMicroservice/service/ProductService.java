328a56a1855b9f343b4f769b2230dc4528f704a9
AndreyGordienkotut <andreykagordienko14@gmail.com>
2025-10-05 20:38:18 +0300

    kafka

--- src/main/java/by/kafkaMicroservice/kafkaMicroservice/service/ProductService.java
+++ src/main/java/by/kafkaMicroservice/kafkaMicroservice/service/ProductService.java
@@ -6,4 +6,5 @@ import java.util.concurrent.ExecutionException;
 
 public interface ProductService {
     String createProduct(CreateProductDto createProductDto) throws ExecutionException, InterruptedException;
+
 }
