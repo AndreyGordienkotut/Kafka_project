529d7c1bb9246daf4279e0fb0a3940e14b7d90cb
AndreyGordienkotut <andreykagordienko14@gmail.com>
2025-10-05 20:37:20 +0300

    kafka

--- EmailNotificationMicroservice/src/main/java/by/emailnotificationmicroservice/emailnotificationmicroservice/EmailNotificationMicroserviceApplication.java
+++ EmailNotificationMicroservice/src/main/java/by/emailnotificationmicroservice/emailnotificationmicroservice/EmailNotificationMicroserviceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailNotificationMicroserviceApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(EmailNotificationMicroserviceApplication.class, args);
     }
---
+++ EmailNotificationMicroservice/src/main/java/by/emailnotificationmicroservice/emailnotificationmicroservice/exception/NonRetryableException.java
@@ -0,0 +1,11 @@
+package by.emailnotificationmicroservice.emailnotificationmicroservice.exception;
+
+public class NonRetryableException extends RuntimeException {
+    public NonRetryableException(String message) {
+        super(message);
+    }
+
+    public NonRetryableException(Throwable cause) {
+        super(cause);
+    }
+}
